b2f67db257012e37dbbd069b393e9c94ab6d0f5c
Ben Chussid <benchussid@Bens-MacBook-Pro.local>
2020-11-01 17:53:15 -0500

    messages are now automatically getting printed to the screen every second

--- client/src/components/DisplayWindow.js
+++ client/src/components/DisplayWindow.js
@@ -1,33 +1,36 @@
 //imports
-import React, { useState, useEffect } from 'react'
-import '../App.css'
+import React, { useState, useEffect } from "react";
+import "../App.css";
 
 function DisplayWindow() {
   //create message and setMsgs variable. Set their states to an empty array in order to receive info later.
-  const [msgs, setMsgs] = useState([])
+  const [msgs, setMsgs] = useState([]);
 
-  //useEffect hook gathers messages from the database and we print them below using .map
+  //useEffect hook gathers messages from the database at specified interval determined in setInterval function and we print them using .map()
   useEffect(() => {
-    //if message length is false / is there is no message length
-    if (!msgs.length) {
-      //get message history from database
-      fetch('/chat/allmessages')
-        .then(res => res.json())
-        .then(msgs => {
-          //read over list of messages in database and update the msg value to include all messages found there
-          setMsgs(msgs)
-        })
-    }
-  }, [])
+    const interval = setInterval(() => {
+      //if message length is false / is there is no message length
+      if (!msgs.length) {
+        //get message history from database
+        fetch("/chat/allmessages")
+          .then((res) => res.json())
+          .then((msgs) => {
+            //read over list of messages in database and update the msg value to include all messages found there
+            setMsgs(msgs);
+          });
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
   //maps over array of message objects and places them in display window div
   return (
     <div id="displaywindow">
-      {msgs.map(msg => {
-        return <p>{msg.sender + ': ' + msg.body}</p>
+      {msgs.map((msg) => {
+        return <p>{msg.sender + ": " + msg.body}</p>;
       })}
     </div>
-  )
+  );
 }
 
 //export to be used in app.js
